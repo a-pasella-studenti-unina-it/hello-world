cb192c5e92a105fd971c58e6f4e00fab830b4335
zAlep99 <77438960+zAlep99@users.noreply.github.com>
2021-01-21 12:44:47 +0100

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -2,9 +2,9 @@
 public class Controller {
 	
 
-	FinestraDisponibilit‡ Disp;
+	FinestraDisponibilit√† Disp;
 	SimulazioneDatabase Sim;
-	public double NuovaDisponibilit‡;
+	public double NuovaDisponibilit√†;
 	public double PrezzoTotale;
 	
 	
@@ -15,36 +15,35 @@ public class Controller {
 	}
 	
 	public Controller() {
-		Disp=new FinestraDisponibilit‡(this);
+		Disp=new FinestraDisponibilit√†(this);
 		Sim= new SimulazioneDatabase(this);
 		Disp.setVisible(true);
 	}
 	
-//	public void RefreshFinestraDisponibilit‡(){
+//	public void RefreshFinestraDisponibilit√†(){
 //		Disp.setVisible(true);
 //		Disp.dispose();
-//		if(Sim.getDisponibilit‡InMagazzino()>0) {
-//			Disp = new FinestraDisponibilit‡(this);
+//		if(Sim.getDisponibilit√†InMagazzino()>0) {
+//			Disp = new FinestraDisponibilit√†(this);
 //			Disp.setVisible(true);
 //		}
 //	}
 //	
 	
 	public String CalcolaPrezzo() {
-		PrezzoTotale=((Disp.getPrezzoAlKiloDouble())*(Disp.getQuantit‡DaSottrarreDouble()));
-		return ""+PrezzoTotale+"Ä";
+		PrezzoTotale=((Disp.getPrezzoAlKiloDouble())*(Disp.getQuantit√†DaSottrarreDouble()));
+		return ""+PrezzoTotale+"‚Ç¨";
 	}
 	
-	public String SottraiQuantit‡() {
-		if(Disp.getQuantit‡DaSottrarreDouble()>Sim.getDisponibilit‡InMagazzino()) {
+	public String SottraiQuantit√†() {
+		if(Disp.getQuantit√†DaSottrarreDouble()>Sim.getDisponibilit√†InMagazzino()) {
 			return"Non ci sono abbastanza scorte";
 		}
 		else {	
-		NuovaDisponibilit‡=((Sim.getDisponibilit‡InMagazzino())-(Disp.getQuantit‡DaSottrarreDouble()));
-//		Disp.setQuantit‡DaSottrarreDouble(0);
-		if(NuovaDisponibilit‡>0) {
-		Sim.setDisponibilit‡InMagazzino(NuovaDisponibilit‡);
-		return ""+NuovaDisponibilit‡;
+		NuovaDisponibilit√†=((Sim.getDisponibilit√†InMagazzino())-(Disp.getQuantit√†DaSottrarreDouble()));
+		if(NuovaDisponibilit√†>0) {
+		Sim.setDisponibilit√†InMagazzino(NuovaDisponibilit√†);
+		return ""+NuovaDisponibilit√†;
 		}
 		else {
 			return"Abbiamo esaurito le scorte in magazzino";	
